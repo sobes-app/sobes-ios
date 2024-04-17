512af67020215687cf09f0cff0f8a7a1db0d032c
Yana Barbashina <yanawishnya@yandex-team.ru>
2024-04-17 19:40:52 +0300

    SBS-8: fix launch screen

---
+++ Sobes/Libraries/UIComponents/Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "UIComponents",
+    platforms: [.iOS(.v16)],
+    products: [
+        .library(
+            name: "UIComponents",
+            targets: ["UIComponents"]),
+    ],
+    targets: [
+        .target(
+            name: "UIComponents",
+            sources: ["Sources"],
+            resources: [.process("Resources")]
+        ),
+    ]
+)
