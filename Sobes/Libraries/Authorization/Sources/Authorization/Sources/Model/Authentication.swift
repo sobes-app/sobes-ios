b5cafe991d715ab218315be0057a64f031264b29
alisa <alisavy2010@gmail.com>
2024-05-11 17:48:31 +0300

    SBS-66: vacancies pagination

--- Sobes/Libraries/Authorization/Sources/Authorization/Sources/Model/Authentication.swift
+++ Sobes/Libraries/Authorization/Sources/Authorization/Sources/Model/Authentication.swift
@@ -18,5 +18,4 @@ public class Authentication: ObservableObject {
 
     private let keychain: Keychain = Keychain(service: "com.swifty.keychain")
     private let accessTokenKey = KeychainKey<String>(key: "accessToken")
-
 }
